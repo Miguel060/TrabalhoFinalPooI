7059f96943884a6392af8b6a6ca96b3e58d875b0
Miguel <migueldoiche2@gmail.com>
2024-12-01 19:56:47 -0300

    Update project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,23 @@
 package application;
 
+import entities.Character;
+import entities.Enums.SorcererWeapons;
+import entities.Enums.WarriorWeapons;
+import entities.Sorcerer;
 import entities.Warrior;
-import entities.Weapons;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args){
-        List<Character> characters = new ArrayList<>();
+    public static void main(String[] args) {
+        Sorcerer mago = new Sorcerer("Orion", SorcererWeapons.FIRE);
+        System.out.println(mago.getName());
+        System.out.println(mago.getHealth());
+        Warrior guerreiro = new Warrior("Brutus", WarriorWeapons.AXE);
+        System.out.println(guerreiro.getName());
+        System.out.println(guerreiro.getHealth());
+        mago.atacar(guerreiro);
+        System.out.println(guerreiro.getHealth());
     }
 }
