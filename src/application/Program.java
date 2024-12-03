f0a104cb0b0a9937d597611218bc0d3a542789f3
Miguel <migueldoiche2@gmail.com>
2024-12-03 00:58:32 -0300

    Update Project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,62 @@
 package application;
 
+import entities.*;
 import entities.Character;
 import entities.Enums.SorcererWeapons;
+import entities.Enums.ThiefWeapons;
 import entities.Enums.WarriorWeapons;
-import entities.Sorcerer;
-import entities.Warrior;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Sorcerer mago = new Sorcerer("Orion", SorcererWeapons.FIRE);
-        System.out.println(mago.getName());
-        System.out.println(mago.getHealth());
-        Warrior guerreiro = new Warrior("Brutus", WarriorWeapons.AXE);
-        System.out.println(guerreiro.getName());
-        System.out.println(guerreiro.getHealth());
-        mago.atacar(guerreiro);
-        System.out.println(guerreiro.getHealth());
+        Scanner scanner = new Scanner(System.in);
+        List<Character> characters = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Escolha o " + (i + 1) + "º personagem: ");
+            System.out.println("1 - Guerreiro\n2 - Bandido\n3 - Mago");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.print("Dê um nome para o " + (i + 1) + "º personagem: ");
+            String nome = scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    Warrior warrior = new Warrior(nome, WarriorWeapons.HAMMER);
+                    characters.add(warrior);
+                    break;
+                case 2:
+                    Thief thief = new Thief(nome, ThiefWeapons.KNIFE);
+                    characters.add(thief);
+                    break;
+                case 3:
+                    Sorcerer sorcerer = new Sorcerer(nome, SorcererWeapons.FIRE);
+                    characters.add(sorcerer);
+                    break;
+                default:
+                    System.out.println("Opção inválida! Escolha novamente.");
+                    i--;
+                    break;
+            }
+        }
+        if (characters.size() == 2) {
+            Character player1 = characters.get(0);
+            Character player2 = characters.get(1);
+
+            System.out.println("\nPersonagens selecionados:");
+            System.out.println("Jogador 1: " + player1.getName() + " - Classe: " + player1.getClass().getSimpleName());
+            System.out.println("Jogador 2: " + player2.getName() + " - Classe: " + player2.getClass().getSimpleName());
+
+            Arena arena = new Arena();
+            arena.duelo(player1, player2);
+        } else {
+            System.out.println("Erro: número insuficiente de personagens para iniciar a batalha.");
+        }
+
+        scanner.close();
     }
 }
